1f76a660d56f71fc34c96c609a051f1ba64148b6
pmgbao2004@gmail.com <pmgbao2004@gmail.com>
2024-11-16 11:19:27 +0700

    done UI

---
+++ src/assets/javascript/tourReservationResult.js
@@ -0,0 +1,9 @@
+
+function edit_tour_button(){
+    window.location.href = "./form_travel_place.html";
+}
+
+function Confirm_button(){
+    alert("Your reservation has been confirmed.");
+    window.location.href = "../../index.html";
+}
