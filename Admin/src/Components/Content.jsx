14d125cd2f4c8d47fd3dc1d17b73873f8eb6ce14
Your Name <thanhdat06112004@gmail.com>
2024-12-21 12:06:32 +0700

    Update dashboard admin

--- Admin/src/Components/Content.jsx
+++ Admin/src/Components/Content.jsx
@@ -1,21 +1,26 @@
 import '../Styles/Home_Content.css';
 import HomeDashboard from './HomeDashboard';
 import AttractionsManagement from './AttractionsManagement';
+import Profile from './Profile'
 import {useNavigate } from 'react-router-dom';
 import UserManagement from './UserManagement';
 import FacilitiesManagement from './FacilitiesManagement';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 const Content = () => {
   const [contentType, setContentType] = useState('home');
+  const [user, setUser] = useState(null); // Lưu thông tin người dùng
+  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate(); // Sử dụng để chuyển hướng trang
   const renderContent = () => {
         if (contentType === 'home') {
             return <HomeDashboard />;
         }
-        else if(contentType === 'attractionsManagement'){
+        else if(contentType === 'profile'){
+          return <Profile />;
+        }else if(contentType === 'attractionsManagement'){
             return <AttractionsManagement />;
         }else if(contentType === 'userManagement'){
             return <UserManagement />;
@@ -23,6 +28,32 @@ const Content = () => {
             return <FacilitiesManagement />;
         };
     };
+
+    useEffect(() => {
+      const fetchAuthentication = async () => {
+          try {
+              const response = await fetch('/authenticate', {
+                  method: 'GET',
+                  credentials: 'include', // Để gửi cookie cùng yêu cầu
+              });
+
+              if (response.ok) {
+                  const data = await response.json();
+                  setUser(data);
+              } else {
+                  setUser(null); // Chưa đăng nhập
+              }
+          } catch (error) {
+              console.error('Error fetching authentication:', error);
+              setUser(null);
+          }finally {
+            setIsLoading(false); // Kết thúc trạng thái tải
+          }
+      };
+
+      fetchAuthentication();
+    }, []);
+  
     const handleLogout = async () => {
       try {
           const response = await fetch('/logout', {
@@ -52,13 +83,17 @@ const Content = () => {
                   <img src="../Images/logoITISE.png" alt="logo" />
           </div>
           <div className="list-select">
-              <div className="dashboard-header list-select-content">
+              <div className="dashboard-header list-select-content" onClick={() => setContentType('profile')}>
                 <div className ="notification">
                   <i class='bx bx-bell'></i>
                 </div>
                 <div className ="admin-info">
                     <h4>Admin</h4>
-                    <p>admin@gmail.com</p>
+                    {isLoading ? (
+                      <p>Loading...</p>
+                    ) : (
+                      <p>{user ? user.accountName : 'Guest'}</p>
+                    )}
                 </div>
               </div>
               <div className="list-select-content" tabIndex="0" onClick={() => setContentType('home')}>
