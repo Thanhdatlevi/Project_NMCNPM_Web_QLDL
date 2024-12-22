358143f0bb60668210264534a6f33bc199a3e607
Thanh Duy <thanhduy190304ptd@gmail.com>
2024-12-22 22:42:12 +0700

    restaurantRoutes

--- Server/src/routes/restaurantRoutes.js
+++ Server/src/routes/restaurantRoutes.js
@@ -11,9 +11,7 @@ router.get('/:resId', RestaurantController.getRestaurantById_tourist)
 //resId: restaurantId
 router.get('/getRelatedrestaurant/:resID', RestaurantController.getRelatedRes);
 
-
 router.get('/by-location/:locationId', RestaurantController.getRestaurantsByLocationId)
-router.get('/by-provider/:providerId', RestaurantController.getRestaurantByProviderId)
 router.get('/api/3-PopularRes', RestaurantController.get_3_PopularRestaurants);
 router.get('/api/getRestaurantsTotal', RestaurantController.getRestaurantsTotal)
 
